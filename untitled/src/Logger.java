c85b02809ffdc826fab43a9393f725e8e178b277
Surya Teja P <suryateja.paduri@ufl.edu>
2022-10-21 18:56:17 -0400

    Added Handshake and Starting remote peers

--- untitled/src/Logger.java
+++ untitled/src/Logger.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+<<<<<<< HEAD
 public class Logger {
     static FileOutputStream file;
     static OutputStreamWriter writer;
@@ -21,25 +22,68 @@ public class Logger {
         try {
             writer.write(s + "\n");
         } catch (Exception ex) {
+=======
+public class Logger
+{
+    static FileOutputStream file;
+    static OutputStreamWriter writer;
+    Logger(String fileName) throws Exception
+    {
+        File dir = new File("logs");
+        dir.mkdir();
+        File logsFile = new File("logs", fileName);
+        file=new FileOutputStream("logs//"+fileName);
+        writer=new OutputStreamWriter(file, StandardCharsets.UTF_8);
+    }
+    public void printLog(String s)
+    {
+        try
+        {
+            writer.write(s+"\n");
+        }
+        catch (Exception ex)
+        {
+>>>>>>> 7a2324b (Added Handshake and Starting remote peers)
             ex.printStackTrace();
             System.out.println(ex.getMessage());
         }
     }
+<<<<<<< HEAD
 
     public void logExit() {
         try {
             writer.flush();
             file.close();
         } catch (Exception ex) {
+=======
+    public  void logExit()
+    {
+        try
+        {
+            writer.flush();
+            file.close();
+        }
+        catch (Exception ex)
+        {
+>>>>>>> 7a2324b (Added Handshake and Starting remote peers)
             ex.printStackTrace();
             System.out.println(ex.getMessage());
         }
     }
+<<<<<<< HEAD
 
     public void logDisplay(String message) {
         Calendar c = Calendar.getInstance();
         SimpleDateFormat d = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
         printLog(d.format(c.getTime()) + " Peer " + message);
         System.out.println(d.format(c.getTime()) + " Peer " + message);
+=======
+    public  void logDisplay(String message)
+    {
+        Calendar c=Calendar.getInstance();
+        SimpleDateFormat d=new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        printLog(d.format(c.getTime())+" Peer "+ message);
+        System.out.println(d.format(c.getTime())+" Peer "+ message);
+>>>>>>> 7a2324b (Added Handshake and Starting remote peers)
     }
 }
---
+++ untitled/src/ParameterInfo.java
@@ -0,0 +1,2 @@
+public class ParameterInfo {
+}
